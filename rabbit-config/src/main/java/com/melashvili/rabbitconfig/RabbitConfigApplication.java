b1c7c0aef633fcb0a8c3445ae586ed9270ac3ec0
Nika Melashvili <109832550+NikaMelashvili@users.noreply.github.com>
2024-04-02 11:52:43 +0400

    rabbitmq config from publisher to a queue

---
+++ rabbit-config/src/main/java/com/melashvili/rabbitconfig/RabbitConfigApplication.java
@@ -0,0 +1,13 @@
+package com.melashvili.rabbitconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitConfigApplication.class, args);
+    }
+
+}
---
+++ rabbit-config/src/test/java/com/melashvili/rabbitconfig/RabbitConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melashvili.rabbitconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
